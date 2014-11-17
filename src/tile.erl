3ac54e870c9c489650e0bb24ac56b3eed280c036
Brecht Gosselé <brecht.gossele@gmail.com>
2014-11-17 20:33:13 +0100

    calculation neighbours + end of board

--- src/tile.erl
+++ src/tile.erl
@@ -1,33 +1,61 @@
 -module(tile).
 
--export([tilemain/1]).
+-export([tilemain/1, format_list/1]).
 
 tilemain( Id ) ->
 	tilemain(Id, 0).
 
 tilemain( Id, Value ) ->
-	ok.
+	tilelife(Id, Value, false).
 
 
 %%%%%%%%%%%%%%%%%
 % fill this out %
 %%%%%%%%%%%%%%%%%
-tilelife()->
+tilelife(Id, CurrentValue, Merged)->
 	receive
 		die ->
 			debug:debug("I, ~p, die.~n",[Id]),
 			exit(killed);
 		up ->
-			ok;
+			Neighbours = neighbours(Id,-4);
 		dn ->
-			ok;
+			Neighbours = neighbours(Id,4);
 		lx ->
-			ok;
+			Neighbours = neighbours(Id,-1);
 		rx ->
-			ok;
+			Neighbours = neighbours(Id,1);
 		{yourValue, Repl} ->
 			Repl ! {tilevalue, Id, CurrentValue, Merged};
 		{setvalue, Future, NewMerged} ->
 			ok
 	end,
-	tilelife( ).
+	tilelife(Id, 42, true).
+
+neighbours(TileNo,F)->
+	[TileNo + F*I || I <- [1,2,3], TileNo + F*I > 0, TileNo + F*I < 17].
+
+end_of_board(Dir, TileNo)->
+	case Dir of
+		up ->
+			TileNo > 12;
+		dn ->
+			TileNo < 5;
+		lx ->
+			TileNo rem 4 == 0;
+		rx ->
+			(TileNo - 1) rem 4 == 0
+	end.
+
+format_list(L) ->
+        io:format("["),
+        fnl(L),
+        io:format("]~n").
+
+fnl([H]) ->
+	io:format("~p", [H]);
+fnl([H|T]) ->
+        io:format("~p,", [H]),
+	fnl(T);
+fnl([]) ->
+        ok.
